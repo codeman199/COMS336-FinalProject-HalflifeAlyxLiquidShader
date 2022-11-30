2b8b358469c58139bc9f190577744cd6b78449ea
Code <codeman199c@gmail.com>
2022-11-30 16:34:42 -0600

    Initial commit

---
+++ Assets/Scripts/Liquid.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Liquid : MonoBehaviour
+{
+    [SerializeField]
+    float MaxWobble = 0.03f;
+    [SerializeField]
+    float WobbleSpeedMove = 1f;
+    [SerializeField]
+    float fillAmount;
+    [SerializeField]
+    float Recovery = 1f;
+    [Range(0, 1)]
+    public float CompensateShapeAmount;
+    [SerializeField]
+    Mesh mesh;
+    Renderer rend;
+    Vector3 pos;
+    Vector3 lastPos;
+    Vector3 velocity;
+    Quaternion lastRot;
+    Vector3 angularVelocity;
+    float wobbleAmountX;
+    float wobbleAmountZ;
+    float wobbleAmountToAddX;
+    float wobbleAmountToAddZ;
+    float pulse;
+    float time = 0.5f;
+    Vector3 comp;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    private void OnValidate()
+    {
+        if (mesh == null)
+        {
+            mesh = GetComponent<MeshFilter>().sharedMesh;
+        }
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+        // decrease wobble over time
+        wobbleAmountToAddX = Mathf.Lerp(wobbleAmountToAddX, 0, Time.deltaTime * (Recovery));
+        wobbleAmountToAddZ = Mathf.Lerp(wobbleAmountToAddZ, 0, Time.deltaTime * (Recovery));
+
+        // make a sine wave of the decreasing wobble
+        pulse = 2 * Mathf.PI * WobbleSpeedMove;
+        wobbleAmountX = wobbleAmountToAddX * Mathf.Sin(pulse * time);
+        wobbleAmountZ = wobbleAmountToAddZ * Mathf.Sin(pulse * time);
+
+        // send it to the shader
+        rend.sharedMaterial.SetFloat("_WobbleX", wobbleAmountX);
+        rend.sharedMaterial.SetFloat("_WobbleZ", wobbleAmountZ);
+
+        // velocity
+        velocity = (lastPos - transform.position) / Time.deltaTime;
+        angularVelocity = GetAngularVelocity(lastRot, transform.rotation);
+
+        // add clamped velocity to wobble
+        wobbleAmountToAddX += Mathf.Clamp((velocity.x + angularVelocity.z) * MaxWobble, -MaxWobble, MaxWobble);
+        wobbleAmountToAddZ += Mathf.Clamp((velocity.z + angularVelocity.x) * MaxWobble, -MaxWobble, MaxWobble);
+
+        // set fill amount
+        Vector3 worldPos = transform.TransformPoint(new Vector3(mesh.bounds.center.x, mesh.bounds.center.y, mesh.bounds.center.z));
+        if (CompensateShapeAmount > 0)
+        {
+            comp = Vector3.Lerp(comp, (worldPos - GetLowestPoint()), Time.deltaTime * 5);
+            pos = worldPos - transform.position - new Vector3(0, fillAmount + (comp.y * CompensateShapeAmount), 0);
+        }
+        else
+        {
+            pos = worldPos - transform.position - new Vector3(0, fillAmount, 0);
+        }
+        rend.sharedMaterial.SetVector("_FillAmount", pos);
+
+        // keep last position
+        lastPos = transform.position;
+        lastRot = transform.rotation;
+    }
+
+    //https://forum.unity.com/threads/manually-calculate-angular-velocity-of-gameobject.289462/#post-4302796
+    Vector3 GetAngularVelocity(Quaternion foreLastFrameRotation, Quaternion lastFrameRotation)
+    {
+        var q = lastFrameRotation * Quaternion.Inverse(foreLastFrameRotation);
+        // no rotation?
+        // You may want to increase this closer to 1 if you want to handle very small rotations.
+        // Beware, if it is too close to one your answer will be Nan
+        if (Mathf.Abs(q.w) > 1023.5f / 1024.0f)
+            return new Vector3(0, 0, 0);
+        float gain;
+        // handle negatives, we could just flip it but this is faster
+        if (q.w < 0.0f)
+        {
+            var angle = Mathf.Acos(-q.w);
+            gain = -2.0f * angle / (Mathf.Sin(angle) * Time.deltaTime);
+        }
+        else
+        {
+            var angle = Mathf.Acos(q.w);
+            gain = 2.0f * angle / (Mathf.Sin(angle) * Time.deltaTime);
+        }
+        return new Vector3(q.x * gain, q.y * gain, q.z * gain);
+    }
+
+    Vector3 GetLowestPoint()
+    {
+        float lowestY = float.MaxValue;
+        Vector3 lowestVert = Vector3.zero;
+        Vector3[] vertices = mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+
+            Vector3 position = transform.TransformPoint(vertices[i]);
+
+            if (position.y < lowestY)
+            {
+                lowestY = position.y;
+                lowestVert = position;
+            }
+        }
+        return lowestVert;
+    }
+}
